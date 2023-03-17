7c143e8be7892892916d8fce58c6769bc07871d1
nishanthvibish18 <nishanthvibish18@gmail.com>
2023-03-17 10:15:27 +0530

    updated code

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors);
 
+const port = 10000;
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+//   });
+// const urlString = "mongodb://127.0.0.1:27017/res"
 
 const urlString = "mongodb+srv://vibish123:vibish123@cluster0.jcfnzmz.mongodb.net/?retryWrites=true&w=majority"
 
@@ -26,8 +31,10 @@ mongoose.connection.once("open", () => {
     console.log("Connection Error:", error);
 });
 
-
-app.listen(() => {
-        console.log('your server is connected successfully.')
-        })
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening on http://0.0.0.0:${port}`);
+  });
+// app.listen(() => {
+//         console.log('your server is connected successfully.')
+//         })
 
